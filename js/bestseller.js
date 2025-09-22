82d5f83a4edc8fc9b5b4ba5e2a0fe154604d21ec
sakthi23102003 <139611638+sakthi23102003@users.noreply.github.com>
2025-09-23 00:11:40 +0800

    Update bestseller.js

--- js/bestseller.js
+++ js/bestseller.js
@@ -85,7 +85,7 @@ const products = [
   }
 ];
 
-function displayProducts(category = "all"){
+function filterProducts(category = "all"){
     const container = document.getElementById("product-container");
     container.innerHTML = "";
     let filtered;
@@ -110,5 +110,6 @@ function displayProducts(category = "all"){
 document.addEventListener("DOMContentLoaded", () => {
   displayProducts("all");
 });
+
 
 
